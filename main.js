6818d068d53b279faca57cf65d2f51a32d9332a6
FunATuns <payton.obrycki@gmail.com>
2019-02-11 21:22:58 -0600

    Added messages

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 var confettiSettings = { target: 'my-canvas' };
 var confetti = new ConfettiGenerator(confettiSettings);
 
+var messageInterval;
+var messageIndex = 0;
+
+function makeid(max) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < max; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
 function suckMyHairyCock() {
+
+
+
   document.getElementById("cunt").style.opacity = "0";
   setTimeout(function() {
-    document.getElementById("cunt").innerHTML = "<div id='leftnut'><div id='rightnut'></div></div>";
+    document.getElementById("cunt").innerHTML = "<div id='leftnut'><div id='rightnut'></div></div><br><div id='message'></div>";
+    messageInterval = setInterval(messageLoop,3000);
     document.getElementById("cunt").style.opacity = "1";
   }, 500);
   setTimeout(function() {
@@ -28,5 +45,29 @@ function suckMyHairyCock() {
     document.getElementById("cunt").innerHTML = "<p id='finalshit'>Congratulations!<br>You've wasted <br>30 seconds of your time!</p>";
     document.getElementById("cunt").style.opacity = "1";
     confetti.render();
+    clearInterval(messageLoop);
   }, 21150);
 }
+
+var messages = [
+    "Grabbing info from Google api...",
+    "Factoring in past snow days...",
+    "Consulting the ECASD superindent...",
+    "Accounting for significant figures...",
+    "",
+    ""
+];
+
+
+function messageLoop() {
+    document.getElementById("message").innerText = messages[messageIndex];
+    document.getElementById("message").style.opacity = "1";
+
+    setTimeout(function() {
+        document.getElementById("message").style.opacity = "0";
+    },2000);
+
+    messageIndex += 1;
+}
+
+document.getElementById("bottomtitty").innerHTML = "Google Weather API Key: "+ makeid(15) +"<br>Sesh ID: " + makeid(7);
